9073404f71da4db459b29abba1aeb856ec0e836e
WATLE <31515524+WATLE@users.noreply.github.com>
2019-07-19 10:11:41 +0900

    Add files via upload
    piyo~

--- www/server.c
+++ www/server.c
@@ -5,12 +5,32 @@ using namespace std;
 #include<string.h>
 #include<unistd.h>
 #include<cstdio>
-//string httpkaesu="HTTP/1.1 200 OK \r\nContent-Type:text/html;charset=utf-8\r\n\r\n<!DOCTYPE html><html><head><title>ネットワークプログラムのwebさいと</title></head><body>ねっとわーくだいすき</body><html>";
+#include<regex.h>
 void HandleTCPCliant(int sock){
+	regex_t regBuf;
+	regmatch_t regMatch[1];
 	char ret[256]="HTTP/1.1 200 OK \r\nContent-Type:text/html;charset=utf-8\r\n\r\n<!DOCTYPE html><html><head><title>ネットワークプログラムのwebさいと</title></head><body>ねっとわーくだいすき</body><html>";
 	char buf[256];
-	int len_r=recv(sock,buf,256,0);
+	char buf2[256];
+	int len_r;
+	const char * pattern="GET[^\\n]+HTTP";
+	if(regcomp(&regBuf,pattern,REG_EXTNDED|REG_NEWLINE)!=0){cerr<<"regcomp miss"<<endl;exit(0);}
+	char result[100];
+	result[0]='\0';
 	
+	while(len_r=recv(sock,buf2,256,0)>0){
+		//なにかする
+		buf2[len_r]='\0';
+		sprintf(buf,"%s",buf2);
+		//\n\r\n\rを検出する
+		if(regexec(&regBuf,buf,regMatch,0)!=0){
+			int startIndex=regMatch[0].rm_so;
+			int endIndex=regMatch[0].rm_eo;
+			if(endIndex-startIndex-9<0){cerr<<"nanika miss"<<endl;exit(0);}
+			strncpy(result,buf+startIndex+4,endIndex-startIndex-9);
+		}
+	}
+	cout<<result<<endl;
 	send(sock,ret,strlen(ret),0);
 }
 int main(int argc,char ** argv){
@@ -32,7 +52,8 @@ int main(int argc,char ** argv){
 	}
 	
 	return 0;
-}//g++ cliant.c -o cliant.exe
+}
+//g++ cliant.c -o cliant.exe
 // ./cliant.exe
 //\\file01.edu-kct.local\home\k16013wi\desktop\ネットワークぷろぐらみんぐ
 /*
